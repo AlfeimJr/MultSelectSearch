44df6259db0b33d82644eb59f131256c88e28924
Gabriel Renovato <gabriel.renovato@docato.com.br>
2024-03-21 23:57:54 -0300

    first commit

--- src/app/mult-select-search/mult-select-search.component.ts
+++ src/app/mult-select-search/mult-select-search.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
+import {MatSelectChange, MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { MatOptionSelectionChange } from '@angular/material/core';
+interface GameItem {
+  name: string;
+  checked: boolean;
+}
 @Component({
   selector: 'app-mult-select-search',
   standalone: true,
@@ -13,27 +17,45 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   styleUrl: './mult-select-search.component.scss'
 })
 export class MultSelectSearchComponent {
-  toppings = new FormControl(['']);
-  gamesListBackup: string[] = ['God of war', 'Street figther 2', 'Super mario', 'Spider man', 'Devil my Cry', 'FF14'];
-  gamesList: string[] = ['God of war', 'Street figther 2', 'Super mario', 'Spider man', 'Devil my Cry', 'FF14'];
+  toppings: FormControl<string[]> = new FormControl([]) as FormControl<string[]>;
+  gamesListBackup: string[] =  ['God of war', 'Street figther 2', 'Super mario', 'Spider man', 'Devil my Cry', 'FF14']
+  gamesList: string[] =  ['God of war', 'Street figther 2', 'Super mario', 'Spider man', 'Devil my Cry', 'FF14']
   searchText: FormControl = new FormControl('');
+  toppingsBackup: string[] = []
 
 
-
-  search(event: KeyboardEvent) {
-    this.gamesList = this.searchText.value
-      ? this.gamesListBackup.filter(
-          (item) => item.toLowerCase().includes(this.searchText.value.toLowerCase())
-        )
-      : this.gamesListBackup.slice();
+  search() {
+    if(this.searchText.value !== ''){
+      this.gamesList = this.gamesListBackup.filter((game) => game.toLowerCase().includes(this.searchText.value.toLowerCase()))
+      
+    }else{
+      this.gamesList = this.gamesListBackup
+    }
   }
 
   setAll(checked: boolean) {
     const allOptions = this.gamesList.slice();
     if (checked) {
       this.toppings.setValue(allOptions);
+      this.toppingsBackup = allOptions
     } else {
       this.toppings.setValue([]);
     }
   }
+
+ select(event: MatOptionSelectionChange) {
+  if(!event.source.active){
+    let backup = this.toppings.value
+    this.toppings.patchValue(backup) 
+    this.toppings.value.push(event.source.value)
+  }else{
+    this.toppings.value.splice(this.toppings.value.indexOf(event.source.value), 1)
+  }
+   
+ }
+
+ selectOn(event: MatSelectChange){
+  this.toppingsBackup.push(event.value[0])
+  this.toppings.setValue(this.toppingsBackup)
+ }
 }
